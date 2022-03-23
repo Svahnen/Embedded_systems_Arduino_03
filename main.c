1d93c6a517a2d629cb8a5270965c01751b5b8a4c
Svahnen <svahn.jonny@gmail.com>
2022-03-23 11:50:57 +0100

    Config ADC

--- main.c
+++ main.c
@@ -14,6 +14,24 @@ int main(void) {
     DDRB &= ~(1 << PB0);  // Set PB0 as input
     // My specific shield config ends here
 
+    // ADC
+
+    // AVCC with external capacitor at AREF pin
+    ADMUX &= ~(1 << REFS0);
+    ADMUX |= (1 << REFS1);
+
+    // MUX: Analog Channel Selection 0000 per default
+
+    // Write one to ADLAR to left adjust the result, page 317 in datasheet
+    ADMUX |= (1 << ADLAR);
+
+    // ADPSn: ADC Prescaler Select [n = 2:0], ADPS[2:0] 011 = 8, page 320 in datasheet
+    ADCSRA |= (1 << ADPS0);
+    ADCSRA |= (1 << ADPS1);
+    ADCSRA &= ~(1 << ADPS2);
+
+    // A single conversion is started by writing a '0' to the Power Reduction ADC bit in the Power Reduction Register (PRR.PRADC), page 307 in datasheet
+
     button_init();
     LED_init();
     uart_init();
